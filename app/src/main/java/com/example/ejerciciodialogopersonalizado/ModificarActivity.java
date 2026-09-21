bc58e77080e7a632137469adf94045da508f92e2
Sofianina18 <snina.work@gmail.com>
2026-09-21 13:39:10 +0200

    creado segunda pantalla de ModificarActivity.java

--- app/src/main/java/com/example/ejerciciodialogopersonalizado/ModificarActivity.java
+++ app/src/main/java/com/example/ejerciciodialogopersonalizado/ModificarActivity.java
@@ -1,13 +1,6 @@
 package com.example.ejerciciodialogopersonalizado;
-//
-//FOto nombre, tipo,
-//tres botones agua , electrico, fuego y cargarlo desde la base de datos
-//boton administrador que lleva a una ventana de crear
-//sobre clcick donde los personajes long click lleva nuevas activitys este caso Modificar
-//y doble click eliminar
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,16 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
-public class MainActivity extends AppCompatActivity {
-
-
+public class ModificarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_modificar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
